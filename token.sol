66a4298bf07d1c24878b9fa1d6707bb176b6411d
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-10-03 23:05:51 +0300

    Update token.sol

--- token.sol
+++ token.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";  //import OZ ERC721 library
 
-
-
+//deriving token contract from OZ contract
 contract LW3Token is ERC20 {
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         _mint(msg.sender, 10 * 10 ** 18); //mint 10 tokens
